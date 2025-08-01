00c4108e00427c1e71572a3ebb957d7c689767a8
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2025-08-01 15:58:55 +0000

    Add a feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,33 @@
 import EleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import pkg from "./package.json" with { type: "json" };
+
+import collections from "./lib/collections/index.js";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyNavigationPlugin);
+
+  eleventyConfig.addPlugin(feedPlugin, {
+    collection: {
+      name: "feed",
+      limit: 10,
+    },
+    metadata: {
+      language: "en-US",
+      title: pkg.name
+        .split(/[\W+]/)
+        .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+        .join(" "),
+      subtitle: pkg.description,
+      base: pkg.author.url,
+      author: {
+        name: pkg.author.name,
+        email: pkg.author.email,
+      },
+    },
+  });
+
+  eleventyConfig.addPlugin(collections);
 
   return {
     dir: {
